dd8156b13d28d42079c282218d236932fd266905
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-05-04 15:15:26 +0200

    metodo post  create

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -7,29 +7,47 @@ namespace la_mia_pizzeria_static.Controllers
     {
         public IActionResult Index()
         {
-            using(PizzaContext pz = new PizzaContext()) {
-            List<Pizza> list = pz.Pizzas.ToList();
-                if(list == null)
-                return View("ErrorList");
-                
-            return View(list);
+            using (PizzaContext pz = new PizzaContext()) {
+                List<Pizza> list = pz.Pizzas.ToList();
+                if (list == null)
+                    return View("ErrorList");
+
+                return View(list);
             }
-   
+
         }
         public IActionResult Details(int id)
         {
-            using(PizzaContext pz = new PizzaContext())
+            using (PizzaContext pz = new PizzaContext())
             {
                 Pizza pizza = pz.Pizzas.Where(p => p.Id == id).FirstOrDefault();
                 return View(pizza);
             }
         }
-        
+
         public IActionResult Create()
         {
-          
-                return View();
-            
+
+            return View();
+
         }
+        [HttpPost]
+        public IActionResult Create(Pizza data)
+        {
+
+            if(!ModelState.IsValid)
+            {
+                return View("Create", data);
+            }
+            using (PizzaContext context = new PizzaContext())
+            {
+                Pizza pz = new Pizza { Name = data.Name, Description = data.Description, Image = data.Image, Price = data.Price };
+                context.Pizzas.Add(pz);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
     }
 }
